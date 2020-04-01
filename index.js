36d33524160da0aa2429307f076b25c667bf6208
Adib Faqih <foezst@gmail.com>
2020-04-01 12:23:13 +0700

    release 1, create getBlock function

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ function generateBoard(row, column) {
     for (let j = 0; j < column; j++) {
       let random = Math.floor(Math.random() * 26);
       let vowRandom = Math.floor(Math.random() * 5);
-      if (Math.random() < 0.3) {
+      if (Math.random() < 0.4) {
         if (!board[i]) {
           board.push([alpha[random]]);
         } else {
@@ -24,4 +24,24 @@ function generateBoard(row, column) {
   
   return board;
 }
-// console.log(generateBoard(4, 6));
+// let theBoard = generateBoard(5, 4);
+
+function getBlock(board) {
+  let vowel = 'AIUEO';
+  let result = 0;
+  let temp;
+
+  for (let i = 0; i < board.length-1; i++) {
+    for (let j = 0; j < board[i].length-1; j++) {
+      temp = board[i][j] + board[i][j+1] + board[i+1][j] + board[i+1][j+1];
+      count = 0;
+      for (let k = 0; k < temp.length; k++) {
+        if (vowel.indexOf(temp[k]) != -1) count++;
+      }
+      if (count === 4) result++;
+    }
+  }
+  return result;
+}
+
+// console.log(getBlock(theBoard));
