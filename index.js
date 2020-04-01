d8ea27d05c16c5053e21c0c2ebc426ae6e73675e
Adib Faqih <foezst@gmail.com>
2020-04-01 11:40:36 +0700

    create generateBoard, release 0

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-
+function generateBoard(row, column) {
+  let alpha = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  let vowel = 'AIUEO';
+  let board = [];
+  for (let i = 0; i < row; i++) {
+    for (let j = 0; j < column; j++) {
+      let random = Math.floor(Math.random() * 26);
+      let vowRandom = Math.floor(Math.random() * 5);
+      if (Math.random() < 0.3) {
+        if (!board[i]) {
+          board.push([alpha[random]]);
+        } else {
+          board[i].push(alpha[random]);
+        }
+      } else {
+        if (!board[i]) {
+          board.push([vowel[vowRandom]]);
+        } else {
+          board[i].push(vowel[vowRandom]);
+        }
+      }
+    }
+  }
+  
+  return board;
+}
+// console.log(generateBoard(4, 6));
