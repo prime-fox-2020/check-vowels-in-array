53334ff86628f4a73d86bedb663101b05f9af445
Eko Prasetyo <ekoprasetyo.me@gmail.com>
2020-04-01 15:54:38 +0700

    Release 0

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
 const createBoard = (rows, cols) => {
-  const dictionary = ['A', 'X', 'C', 'Y', 'E', 'O', 'O', 'S', 'I', 'U', 'I', 'N', 'M', 'Y', 'O', 'E', 'P', 'D', 'A', 'I']
+  const dictionary = ['A', 'X', 'C', 'Y', 'E', 'O', 'O', 'S', 'I', 'U', 'I', 'N', 'M', 'Y', 'O', 'E', 'P', 'D', 'A', 'I'];
+  const vowels = {
+    A: true,
+    I: true,
+    U: true,
+    E: true,
+    O: true
+  };
+
   let board = [],
-      index = 0;
+      index = 0,
+      total = 0;
   
   for (let i = 0; i < rows; i++) {
     let temp = [];
@@ -12,7 +21,13 @@ const createBoard = (rows, cols) => {
     board.push(temp);
   }
 
-  return board;
+  for (let i = 0; i < rows - 1; i++) {
+    for (let j = 0; j < cols - 1; j++) {
+      if (vowels[board[i][j]] && vowels[board[i][j + 1]] && vowels[board[i + 1][j]] && vowels[board[i + 1][j + 1]] ) total++;
+    }
+  }
+  console.log(board);
+  return total;
 }
 
 
