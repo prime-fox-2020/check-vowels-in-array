ab190b302b11f0c8bcaa4044f53251b97d2cfcfc
Eko Prasetyo <ekoprasetyo.me@gmail.com>
2020-04-01 16:23:52 +0700

    Release 1 dengan peningkatan kesempatan huruf vokal

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const createBoard = (rows, cols) => {
-  const dictionary = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  const dictionary = 'BCDFGHJKLMNPQRSTVWXYZ';
+  const vowelsDictionary = 'AIUEO';
   const vowels = {
     A: true,
     I: true,
@@ -15,8 +16,13 @@ const createBoard = (rows, cols) => {
   for (let i = 0; i < rows; i++) {
     let temp = [];
     for (let j = 0; j < cols; j++) {
-      index = Math.floor(Math.random() * 26);
-      temp.push(dictionary[index]);
+      if (Math.floor(Math.random() * 2)) {
+        index = Math.floor(Math.random() * 5);
+        temp.push(vowelsDictionary[index]);
+      } else {
+        index = Math.floor(Math.random() * 21);
+        temp.push(dictionary[index]);
+      }
     }
     board.push(temp);
   }
@@ -26,7 +32,7 @@ const createBoard = (rows, cols) => {
       if (vowels[board[i][j]] && vowels[board[i][j + 1]] && vowels[board[i + 1][j]] && vowels[board[i + 1][j + 1]] ) total++;
     }
   }
-  console.log(board);
+  // console.log(board);
   return total;
 }
 
