110b89a6a3843446c7215710ae43160127a2ffd0
Vincent Guizot <30890039+CenZo128@users.noreply.github.com>
2020-04-01 13:15:55 +0700

    Fixed

--- index.js
+++ index.js
@@ -1,1 +1,47 @@
+let getAlphabeth = () => {
+    let num = Math.floor(Math.random()*26)
 
+    var huruf = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    return huruf[num]
+}
+let checkVowels = (x) => {
+    let vowels = ['A','I','U','E','O'], check
+    for(let i=0;i<vowels.length;i++){
+        if(x==vowels[i]) check=true 
+    }
+    return (check) ? true : false
+}
+let generateBoard = (row,column) => {
+    let arr=[],temp=[]
+    for (let i = 0; i < row; i++) {
+        temp=[]
+        for (let j = 0; j < column; j++) {
+            temp.push(getAlphabeth())
+        }
+        arr.push(temp)
+    }
+    return arr
+}
+let checkBoard = (board) => {
+    let count=0
+    for (let i = 0; i < board.length-1; i++) {
+        for (let j = 0; j < board[i].length-1; j++) {
+            if(checkVowels(board[i][j])==true && checkVowels(board[i][j+1])==true && checkVowels(board[i+1][j])==true && checkVowels(board[i+1][j+1])==true ) count++
+        }
+        
+    }
+    return count
+}
+let main = (a,b) => {
+    let board = generateBoard(a,b)
+    // board = [
+    //     ['A','I','C'],
+    //     ['I','O','E'],
+    //     ['B','U','A']
+    // ]
+    let total = checkBoard(board)
+
+    return [board,total]
+}
+
+console.log(main(5,3))
