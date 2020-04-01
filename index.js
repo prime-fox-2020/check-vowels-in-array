6df361219507804fa0f9892681e97460387dcda5
Eko Prasetyo <ekoprasetyo.me@gmail.com>
2020-04-01 15:38:35 +0700

    base case

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
+const createBoard = (rows, cols) => {
+  const dictionary = ['A', 'X', 'C', 'Y', 'E', 'O', 'O', 'S', 'I', 'U', 'I', 'N', 'M', 'Y', 'O', 'E', 'P', 'D', 'A', 'I']
+  let board = [],
+      index = 0;
+  
+  for (let i = 0; i < rows; i++) {
+    let temp = [];
+    for (let j = 0; j < cols; j++) {
+      temp.push(dictionary[index]);
+      index++;
+    }
+    board.push(temp);
+  }
 
+  return board;
+}
+
+
+
+console.log(createBoard(5,4));
