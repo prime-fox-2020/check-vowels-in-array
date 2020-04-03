e85c350107307b9133837e0c9bb2e2dab2574a69
Raka Dhaneswara <58630970+rakdhan@users.noreply.github.com>
2020-04-03 10:34:00 +0700

    update tata penulisan

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-
-// const vowelsInMatrix = (mtx) => {
 const vowelsInMatrix = (baris, kolom) => { 
     let alph = 'AAAABCDEEEEFGHIIIIJKLMNOOOOPQRSTUUUUVWXYZ'
     let vokal = 'AIUEO'
@@ -12,23 +10,20 @@ const vowelsInMatrix = (baris, kolom) => {
             // 26: maximize 'vokal' chances with originial amount of alphabets
         }
         mtx.push(temp)
-    }   console.log(mtx)
+    }   
+    console.log(mtx)
     for (let i = 0; i < mtx.length-1; i++) {
         for (let k = 0; k < mtx[i].length-1; k++) {
             let cek = 0
             let kombinasi = [mtx[i][k], mtx[i][k+1], mtx[i+1][k], mtx[i+1][k+1]]//mtx2*2
             for (let m = 0; m < kombinasi.length; m++) {
-                if (vokal.includes(kombinasi[m].toUpperCase())) cek++;
-            }   if (cek === kombinasi.length) countMtx++;
+                if (vokal.includes(kombinasi[m].toUpperCase()))
+                    cek++;
+            }   
+            if (cek === kombinasi.length) countMtx++;
         }        
-    }   return `${countMtx} matrix 2x2 full of vocal alphabets`
+    }   
+    return `${countMtx} matrix 2x2 full of vocal alphabets`
 }
 console.log(vowelsInMatrix(5,3));
-// var fake = [
-//     ['A','i','B','S','D'],
-//     ['U','E','C','T','Y'],
-//     ['O','A','T','B','J']
-// ]
-// console.log(vowelsInMatrix(fake));
-
 
